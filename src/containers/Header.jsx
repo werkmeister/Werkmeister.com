7c121b77f4cdb8f029895355d37a644e9416dbb9
Joshua Werkmeister <jwerkme@gmail.com>
2015-08-11 10:36:47 -0500

    contact component events

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-import { Link }             from 'react-router';
+// import { Link }             from 'react-router';
 // import logo                 from '../images/logo.svg';
 // import logo                 from '../images/cursivelogo.svg';
 import styles               from './header.css';
+import Contact              from '../pages/contact';
 // import styles               from '../styles/components.css';
 
         // <Link to="home"><img src={logo}/></Link>
+          // <Link to="contact">Contact</Link>
 export default class Header extends Component {
+  constructor() {
+    super();
+    this.state = {display: false};
+  }
+  openContact = e => {
+    e.preventDefault();
+    this.setState({display: true});
+  }
+  closeContact = e => {
+    e.preventDefault();
+    this.setState({display: false});
+  }
+
   render() {
     return (
       <div className={styles.header}>
         <div className={styles.container}>
-          <Link to="contact">Contact</Link>
+          <a href="#" onClick={this.openContact.bind(this)}>Contact</a>
+          {this.state.display ?
+            <Contact close={this.closeContact.bind(this)}/>
+          : null}
         </div>
       </div>
     );
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import styles               from './contact.css';
 
 export default class Contact extends Component {
+  static propTypes = {
+    close: React.PropTypes.func.isRequired
+  }
+
   render() {
     return (
-      <section>
-        This is contact
+      <section className={styles.contact}>
+        <i onClick={this.props.close} className="fa fa-times"></i>
+        <input type="text" placeholder="Name"/>
+        <input type="text" placeholder="Organization"/>
+        <input type="text" placeholder="Message"/>
       </section>
     );
   }
