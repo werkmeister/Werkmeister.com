29300b83b47db3be934a32eb6b4809bc7c155210
Joshua Werkmeister <jwerkme@gmail.com>
2015-08-12 15:51:44 -0500

    move header events

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,37 +1,36 @@
 import '../styles/main.css';
+import styles                 from './app.css';
 import React, { Component }   from 'react';
 import { RouteHandler }       from 'react-router';
 import Navigation             from './Navigation';
 import Header                 from './Header';
-import Tools                from '../pages/tools';
+import Tools                  from '../pages/tools';
+import Contact                from '../pages/contact';
 
 export default class App extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {background: false};
-  // }
-  // show() {
-  // }
-
-  // handleResize() {
-  //   if ( window.scrollY >= window.innerHeight) {
-  //     this.setState({background: !this.state.background});
-  //   }
-  // }
-  // componentDidMount() {
-  //   window.addEventListener('scroll', this.handleResize);
-  // }
-  // componentWillUnmount() {
-  //   window.removeEventListener('scroll', this.handleResize);
-  // }
+  constructor() {
+    super();
+    this.state = {display: false};
+  }
+  openContact = e => {
+    e.preventDefault();
+    this.setState({display: true});
+  }
+  closeContact = e => {
+    e.preventDefault();
+    this.setState({display: false});
+  }
 
   render() {
     return (
-      <div>
+      <div className={this.state.display ? styles.container : null}>
       <RouteHandler />
       <Tools />
       <Navigation />
-      <Header />
+      <Header open={this.openContact.bind(this)}/>
+      {this.state.display ?
+        <Contact close={this.closeContact.bind(this)}/>
+      : null}
       </div>
     );
   }
--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 import styles               from './header.css';
-import Contact              from '../pages/contact';
 
 export default class Header extends Component {
-  constructor() {
-    super();
-    this.state = {display: false};
-  }
-  openContact = e => {
-    e.preventDefault();
-    this.setState({display: true});
-  }
-  closeContact = e => {
-    e.preventDefault();
-    this.setState({display: false});
+  static propTypes = {
+    open: React.PropTypes.func.isRequired
   }
 
   render() {
     return (
       <div className={styles.header}>
         <div className={styles.container}>
-          <a href="#" onClick={this.openContact.bind(this)}>Contact</a>
-          {this.state.display ?
-            <Contact close={this.closeContact.bind(this)}/>
-          : null}
+          <a href="#" onClick={this.props.open}>Contact</a>
         </div>
       </div>
     );
