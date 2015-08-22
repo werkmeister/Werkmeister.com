8cb9809a23cd0be988117fcdea28c2d3631a65d6
Joshua Werkmeister <jwerkme@gmail.com>
2015-08-22 17:07:43 -0500

    small changes to contact page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -9,15 +9,21 @@ export default class Contact extends Component {
   render() {
     return (
       <section className={styles.contact}>
-        <i onClick={this.props.close} className="fa fa-times"></i>
-        <p>Let's talk business</p>
-        <input type="text" placeholder="Name"/>
-        <input type="text" placeholder="Organization"/>
-        <input type="text" placeholder="Message"/>
-        <div style={{textAlign: 'center'}}>
-          <button form="emailForm" className={styles.submit}>
-            <i className="fa fa-paper-plane-o"></i>
-          </button>
+        <div style={{
+            maxWidth: '980px',
+            textAlign: 'right',
+            margin: 'auto',
+            padding: '20px'}}>
+          <i onClick={this.props.close} className="fa fa-times"></i>
+          <p>Let's talk business</p>
+          <input type="text" placeholder="Name"/>
+          <input type="text" placeholder="Organization"/>
+          <input type="text" placeholder="Message"/>
+          <div style={{textAlign: 'center'}}>
+            <button form="emailForm" className={styles.submit}>
+              <i className="fa fa-paper-plane-o"></i>
+            </button>
+          </div>
         </div>
       </section>
     );
